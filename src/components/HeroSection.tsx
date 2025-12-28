fd8762f61744f70d9dca97744dd7a412f919654c
rohan <rohanrock986@gmail.com>
2025-12-28 10:56:53 +0530

    updated HeroSection

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -47,7 +47,7 @@ const HeroSection = () => {
               isVisible ? "opacity-100 translate-x-0" : "opacity-0 translate-x-12"
             }`}
           >
-            I'M A WEBSITE DEVELOPER
+            I'M A WEBSITE DEVELOPER & CONTENT CREATOR 
           </p>
 
           {/* CTA Button */}
